a7a4ac53e0d63d1be973919ab7d4af79101faf64
shota <tontonka2@gmail.com>
2024-05-31 05:14:53 +0900

    finish 05

---
+++ spec/controllers/tasks_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe TasksController, type: :controller do
+  before do
+    @user = FactoryBot.create(:user)
+    @project = FactoryBot.create(:project, owner: @user)
+    @task = @project.tasks.create!(name: 'Test task')
+  end
+
+  describe '#create' do
+    it 'respondes with JSON formatted output' do
+      new_task = { name: 'new test tasks' }
+      sign_in @user
+      post :create, format: :json,
+        params: { project_id: @project.id, task: new_task }
+      expect(response.content_type).to include 'application/json'
+    end
+
+    it 'adds a new task to the project' do
+      new_task = { name: 'new test task' }
+      sign_in @user
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.to change(@project.tasks, :count).by (1)
+    end
+
+    it 'requires authentication' do
+      new_task = { name: 'new test task' }
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.to_not change(@project.tasks, :count)
+      expect(response).to_not be_successful
+    end
+  end
+end
