ed3d70793b40d236dc923ddc3312cb95f3eea012
shota <tontonka2@gmail.com>
2024-06-02 07:07:10 +0900

    add edit project spec

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -1,22 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
-  scenario 'user creates a new project' do
-    user = FactoryBot.create(:user)
+  let(:user) { FactoryBot.create(:user) }
+
+  describe '#new' do
+    scenario 'user creates a new project' do
+      sign_in_and_vist_root_path(user)
+
+      expect {
+        click_link 'New Project'
+        fill_in 'Name', with: 'Test Project'
+        fill_in 'Description', with: 'Trying out Capybara'
+        click_button 'Create Project'
+      }.to change(user.projects, :count).by 1
+
+      aggregate_failures do
+        expect(page).to have_content 'Project was successfully created'
+        expect(page).to have_content 'Test Project'
+        expect(page).to have_content "Owner: #{user.name}"
+      end
+    end
+  end
+
+  describe '#edit' do
+    let!(:project) {
+      FactoryBot.create(:project,
+        name: 'First Project',
+        description: 'first step',
+        owner: user)
+    }
+
+    scenario 'user edits the project' do
+      sign_in_and_vist_root_path(user)
+
+      click_link 'First Project'
+      click_link 'Edit'
+
+      fill_in 'Description', with: 'final step'
+      click_button 'Update Project'
+
+      expect(page).to have_content 'Project was successfully updated.'
+      expect(page).to have_content 'final step'
+    end
+  end
+
+  def sign_in_and_vist_root_path(user)
     sign_in user
     visit root_path
-
-    expect {
-      click_link 'New Project'
-      fill_in 'Name', with: 'Test Project'
-      fill_in 'Description', with: 'Trying out Capybara'
-      click_button 'Create Project'
-    }.to change(user.projects, :count).by 1
-
-    aggregate_failures do
-      expect(page).to have_content 'Project was successfully created'
-      expect(page).to have_content 'Test Project'
-      expect(page).to have_content "Owner: #{user.name}"
-    end
   end
 end
