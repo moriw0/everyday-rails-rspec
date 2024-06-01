5bb9e38fcda98c8f21a8455247f948e2dd74473d
shota <tontonka2@gmail.com>
2024-06-01 18:43:09 +0900

    use let

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,18 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  before do
-    @user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'tester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze',
-    )
-
-    @project = @user.projects.create(
-      name: 'Test Project'
-    )
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
 
   it 'generates associated data from a factory' do
     note = FactoryBot.create(:note)
@@ -23,8 +13,8 @@ RSpec.describe Note, type: :model do
   it 'is valid with a user, project, and message' do
     note = Note.new(
       message: 'This is a simple note.',
-      user: @user,
-      project: @project
+      user: user,
+      project: project
     )
     expect(note).to be_valid
   end
@@ -36,31 +26,35 @@ RSpec.describe Note, type: :model do
   end
 
   describe 'search message for a term' do
-    before do
-      @note1 = @project.notes.create(
-        message: 'This is the first note.',
-        user: @user
-      )
-      @note2 = @project.notes.create(
-        message: 'This is the second note.',
-        user: @user
-      )
-      @note3 = @project.notes.create(
-        message: 'First, preheat the oven.',
-        user: @user
-      )
-    end
+    let!(:note1) { FactoryBot.create(:note,
+      project: project,
+      message: 'This is the first note.',
+      user: user
+    ) }
+
+    let!(:note2) { FactoryBot.create(:note,
+      project: project,
+      message: 'This is the second note.',
+      user: user
+    ) }
+
+    let!(:note3) { FactoryBot.create(:note,
+      project: project,
+      message: 'First, preheat the oven.',
+      user: user
+    ) }
 
     context 'when a match is found' do
       it 'returns notes that match the search term' do
-        expect(Note.search('first')).to include(@note1, @note3)
-        expect(Note.search('first')).to_not include(@note2)
+        expect(Note.search('first')).to include(note1, note3)
+        expect(Note.search('first')).to_not include(note2)
       end
     end
 
     context 'when no match is found' do
       it 'returns an empty collection when no results are found' do
         expect(Note.search('message')).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
