e7749873a4a592c1a7dc55efa10c363dffada865
shota <tontonka2@gmail.com>
2024-06-04 05:17:57 +0900

    finish 10

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,12 +38,21 @@ RSpec.describe User, type: :model do
   # def send_welcome_email
   #   UserMailer.welcome_email(self).deliver_later
   # end
-  it 'sends a welcome email on account creation',focus:true do
+  it 'sends a welcome email on account creation' do
     # メソッドチェーンを許可する
     allow(UserMailer).to \
       receive_message_chain(:welcome_email, :deliver_later)
     user = FactoryBot.create(:user)
     # ユーザー作成時に既に実行したことを検証
     expect(UserMailer).to have_received(:welcome_email).with(user)
+  end
+
+  it 'performs geododing', vcr: true do
+    user = FactoryBot.create(:user, last_sign_in_ip: '161.185.207.20')
+    expect {
+      user.geocode
+  }.to change(user, :location).
+    from(nil).
+    to('New York City, New York, US')
   end
 end
