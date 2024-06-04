573195f0385e3a8ab6f6631b3905abfc2718f4a5
shota <tontonka2@gmail.com>
2024-06-04 10:06:04 +0900

    add project complete function

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -44,6 +44,27 @@ RSpec.describe "Projects", type: :system do
     end
   end
 
+  describe '#complete' do
+    let!(:project) {
+      FactoryBot.create(:project,
+        name: 'First Project',
+        owner: user)
+    }
+
+    scenario 'user completes a project' do
+      sign_in_and_vist_root_path(user)
+
+      visit project_path(project)
+      expect(page).to_not have_content "Completed"
+      click_button "Complete"
+      expect(project.reload.completed?).to be true
+      expect(page).to \
+      have_content "Congratulations, this project is complete!"
+      expect(page).to have_content "Completed"
+      expect(page).to_not have_button "Complete"
+    end
+  end
+
   def sign_in_and_vist_root_path(user)
     sign_in user
     visit root_path
